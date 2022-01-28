fb05eed2d12e27ce1ac770ddb8188121404d30da
xiuyuan0216 <x1yuan@ucsd.edu>
2022-01-28 11:58:57 -0800

    the variable expect is not instantiated

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,6 +7,10 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class MarkdownParseTest {
+    @Before
+    public void setUp(){
+        List<String> expect = List.of("https://something.com", "some-page.html");
+    }
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -15,7 +19,7 @@ public class MarkdownParseTest {
     @Test
     public void testFile1() throws IOException {
         String contents= Files.readString(Path.of("/Users/xiuyuan/Documents/GitHub/markdownparse/test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
+        
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
     
