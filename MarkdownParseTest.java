185aa8c257fe6a797d0d0e4dc7e47c39ec8fc9dd
xiuyuan0216 <97642080+xiuyuan0216@users.noreply.github.com>
2022-02-03 18:46:32 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,6 +12,7 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    /*
     @Test
     public void testGetLinks1() throws IOException{
         String file=Files.readString(Path.of("test-file.md"));
@@ -47,4 +48,5 @@ public class MarkdownParseTest {
     public void willFail(){
         assertEquals(1, 1);
     }
+    */
 }
