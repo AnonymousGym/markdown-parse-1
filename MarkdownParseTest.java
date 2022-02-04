69907394f7acd1be05938e115ecc0f1b02103ded
xiuyuan0216 <97642080+xiuyuan0216@users.noreply.github.com>
2022-02-03 18:45:36 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,28 +1,50 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class MarkdownParseTest {
-    List<String> expect;
-    @Before
-    public void setUp(){
-        expect = List.of("https://something.com", "some-page.html");
-    }
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-    /*
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("/Users/xiuyuan/Documents/GitHub/markdownparse/test-file.md"));
-        
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testGetLinks1() throws IOException{
+        String file=Files.readString(Path.of("test-file.md"));
+        assertEquals(MarkdownParse.getLinks(file), List.of("https://something.com","some-page.html"));
     }
-    */
-    
+    @Test
+    public void testGetLinks2() throws IOException{
+        String file=Files.readString(Path.of("test-file2.md"));
+        assertEquals(MarkdownParse.getLinks(file), 
+        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.google.com/maps"));
+    }
+    @Test
+    public void testGetLinks3() throws IOException{
+        String file=Files.readString(Path.of("test-file3.md"));
+        assertEquals(MarkdownParse.getLinks(file), 
+        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+    }
+    @Test
+    public void testGetLinksBulk() throws IOException{
+        String file=Files.readString(Path.of("test-file4.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        file=Files.readString(Path.of("test-file6.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        file=Files.readString(Path.of("test-file7.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        //tests below are bugged
+        file=Files.readString(Path.of("test-file8.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of("a link on the first line"));
+        file=Files.readString(Path.of("test-file5.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+    }
+    @Test
+    public void willFail(){
+        assertEquals(1, 1);
+    }
 }
