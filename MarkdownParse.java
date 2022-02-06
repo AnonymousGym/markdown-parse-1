654960ec0bf3c9ba1e8eae74ed21c2661de7eea8
AnonymousGym <97484485+AnonymousGym@users.noreply.github.com>
2022-02-06 00:32:40 -0800

    infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,40 +15,16 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket <0 ) break;
+
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            if(nextCloseBracket < 0) break;
+
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            if(openParen <0) break;
+
             int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen <0) break;
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-        }
-        return toReturn;
-    }
-    public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-        System.out.println(getLinksRefined(contents));
-        
-    }
-    public static ArrayList<String> getLinksRefined(String markdown){
-        ArrayList<String> toReturn = new ArrayList<>();
-        Stack<Integer> result = new Stack<>();
-        for(int i = 0; i<markdown.length();i++){
-            if(markdown.charAt(i) == '('){
-                result.add(i);
-            }
-            if(markdown.charAt(i)==')'){
-                if(result.isEmpty()){
-                    break;
-                }
-                int a = result.pop();
-                toReturn.add(markdown.substring(a+1,i));
-            }
+            System.out.println("Value of currentIndex at end of loop: "+currentIndex);
         }
         return toReturn;
     }
